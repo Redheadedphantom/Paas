075c98b22d3ba6b7c9eb19b3c829aa1c8a40b99b
Mustafa Malviwala <102210999+Redheadedphantom@users.noreply.github.com>
2023-11-13 07:26:51 +0000

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -87,7 +87,7 @@ export default function Home() {
             Deploy <span>-&gt;</span>
           </h2>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with 
+            Instantly deploy your Next.js site to a shareable URL with Vercels
           </p>
         </a>
       </div>
