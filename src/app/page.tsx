49caa95fa360f469e74d163451e189d91cced89c
Mustafa Malviwala <102210999+Redheadedphantom@users.noreply.github.com>
2023-11-13 07:32:43 +0000

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,7 +50,7 @@ export default function Home() {
           <h2>
             Docs <span>-&gt;</span>
           </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
+          <p>find the </p>
         </a>
 
         <a
