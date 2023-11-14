be6f79f778577119e853b3367454baf0380a8a51
Mustafa Malviwala <102210999+Redheadedphantom@users.noreply.github.com>
2023-11-14 10:04:57 +0000

    commit

---
+++ src/app/MainContent.tsx
@@ -0,0 +1,13 @@
+// MainContent.tsx
+import React from 'react';
+
+const MainContent: React.FC = () => {
+  return (
+    <div style={{ flex: 1, padding: '20px' }}>
+      <h2>Main Content</h2>
+      {/* Add main content components */}
+    </div>
+  );
+};
+
+export default MainContent;
