4ce2fc0bbb91aa2f8e750ffbb1a4e8f9dd417203
Mustafa Malviwala <102210999+Redheadedphantom@users.noreply.github.com>
2023-11-14 10:03:55 +0000

    commit

---
+++ src/app/Sidebar.tsx
@@ -0,0 +1,13 @@
+// Sidebar.tsx
+import React from 'react';
+
+const Sidebar: React.FC = () => {
+  return (
+    <div style={{ width: '200px', backgroundColor: '#333', color: '#fff', padding: '20px' }}>
+      <h2>Sidebar</h2>
+      {/* Add sidebar links or components */}
+    </div>
+  );
+};
+
+export default Sidebar;
