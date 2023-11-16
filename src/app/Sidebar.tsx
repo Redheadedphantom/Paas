e59fd3070836e3b35dbdcd73fef2c2f62ee39125
Mustafa Malviwala <102210999+Redheadedphantom@users.noreply.github.com>
2023-11-16 07:08:08 +0000

    commit

--- src/app/Sidebar.tsx
+++ src/app/Sidebar.tsx
@@ -1,4 +1,3 @@
-// Sidebar.tsx
 import React from 'react';
 
 const Sidebar: React.FC = () => {
@@ -6,6 +5,24 @@ const Sidebar: React.FC = () => {
     <div style={{ width: '200px', backgroundColor: '#333', color: '#fff', padding: '20px' }}>
       <h2>Sidebar</h2>
       {/* Add sidebar links or components */}
+      <ul style={{ listStyle: 'none', padding: 0 }}>
+        <li style={{ marginBottom: '10px' }}>
+          <a href="#dashboard" style={{ textDecoration: 'none', color: '#fff' }}>
+            Dashboard
+          </a>
+        </li>
+        <li style={{ marginBottom: '10px' }}>
+          <a href="#analytics" style={{ textDecoration: 'none', color: '#fff' }}>
+            Analytics
+          </a>
+        </li>
+        <li style={{ marginBottom: '10px' }}>
+          <a href="#settings" style={{ textDecoration: 'none', color: '#fff' }}>
+            Settings
+          </a>
+        </li>
+        {/* Add more menu items as needed */}
+      </ul>
     </div>
   );
 };
