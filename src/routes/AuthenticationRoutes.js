ef6b40029bc5c3ec988cd07b1bab46769ea1d1b6
Mustafa Malviwala <102210999+Redheadedphantom@users.noreply.github.com>
2024-01-02 10:39:33 +0000

    commit

--- src/routes/AuthenticationRoutes.js
+++ src/routes/AuthenticationRoutes.js
@@ -13,6 +13,16 @@ const AuthRegister3 = Loadable(lazy(() => import('views/pages/authentication/aut
 const AuthenticationRoutes = {
   path: '/',
   element: <MinimalLayout />,
-
+  children: [
+    {
+      path: '/pages/login/login3',
+      element: <AuthLogin3 />
+    },
+    {
+      path: '/pages/register/register3',
+      element: <AuthRegister3 />
+    }
+  ]
+};
 
 export default AuthenticationRoutes;
